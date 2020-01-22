f06254cc9ad1321de0bbc700d0182c3ceccbcac0
abhishekkb <abhishekkb@users.noreply.github.com>
2020-01-22 11:59:20 -0600

    slightly modified solution

--- problem001/Solution01.java
+++ problem001/Solution01.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution01{
   public static void main(String[] args){
   
@@ -6,7 +9,61 @@ public class Solution01{
   
   
   public static String solve(String input){
-  
+    String[] inputs = input.split("\n");
+    int rows = Integer.parseInt(inputs[0]);
+    
+    String myStr = inputs[1];
+    if(rows ==1) {
+    	return myStr;
+    }
+    
+    List<List<Character>> zigZag = new ArrayList<>();
+    for(int i=0;i<rows; i++) {
+    	zigZag.add(new ArrayList<>());
+    }
+    boolean isZig = true;
+    
+    int r=0;
+    
+    for (int i = 0; i < myStr.length(); i++) {
+		List<Character> rowStr = zigZag.get(r);
+		//zig
+		if(isZig) {
+			if(rows ==2) {
+				rowStr.add(myStr.charAt(i));
+				r++;
+				isZig = false;
+			}else {
+				rowStr.add(myStr.charAt(i));
+				if(r== rows-1) {
+					isZig = false;
+					r = rows-2;
+				}else {
+					r++;
+				}
+			}
+		}
+		//zag
+		else {
+			if(rows ==2) {
+				rowStr.add(myStr.charAt(i));
+				r--;
+				isZig = true;
+			}else {
+				rowStr.add(myStr.charAt(i));
+				if(r==1) {
+					isZig = true;
+				}
+				r--;
+			}
+		}
+	}
+
+    StringBuilder outBuilder = new StringBuilder();
+    zigZag.forEach(currRow->currRow.forEach(outBuilder::append));
+    
+	return outBuilder.toString();
   
   }
 }
+
